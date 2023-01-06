0b3c0f3b36cdd678f6a867b9f31a48e9b887a9a0
GRMoura55 <gabriel.rodrigues@kahsh.com>
2023-01-06 12:29:32 -0300

    fix: ambiente de desenvolvimento

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
+
 <?php
 
-phpinfo();
+include('autoload.php');
+
+$classes = new Classes();
+
+
+$consulta = "SELECT * FROM teste";
+$resultado = $classes->connect([$consulta,'Query']);
+
+while ($row = $classes->connect([$resultado[0],'Fetch'])) {
+	echo $row['id'];
+}
+?>
